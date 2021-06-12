a1f5a54a15a1ad55337897cad4be8742b97dc9af
try-catch-stack <77742477+try-catch-stack@users.noreply.github.com>
2021-06-12 12:35:08 +0530

    Add minimax algorithm to the single player mode

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var origBoard;
-const huPlayer = 'O';
-const aiPlayer = 'X';
+let player1;
+let player2;
 let player;
 let choicesIndex;
 let gameOver;
@@ -11,16 +11,36 @@ const winCombos = [
 	[0, 3, 6],
 	[1, 4, 7],
 	[2, 5, 8],
-    [2, 4, 6],
+  [2, 4, 6],
 	[0, 4, 8]
 	
 ]
 
 const cells = document.querySelectorAll('.cell');
-// startGame();
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+  
+
+function home() {
+    document.getElementById("table-container").style.display = "none";
+    document.getElementById("startPage").style.display = "flex";
+	document.getElementById("endPage").style.display = "none";
+}
+
 
 function startGame() {
-    // console.log($(' label.active input').val())
+
+    if(document.getElementById("X").checked){
+        player1="X" 
+        player2="O"
+    }else{
+        player1="O"
+        player2="X"
+    }
+
     choicesIndex=[0,1,2,3,4,5,6,7,8];
     gameOver=false;
     document.getElementById("table-container").style.display = "flex";
@@ -35,32 +55,28 @@ function startGame() {
 }
 
 function turnClick(square) {
-	// turn(square.target.id, huPlayer)
+	
     turn(square.target.id)
 }
 
-// function turn(squareId, player) {
-// 	origBoard[squareId] = player;
-// 	document.getElementById(squareId).innerText = player;
-    
-// }
 
 function turn(squareId) {
-	
-    // if (player===huPlayer){
-    // player=aiPlayer;
-    // choicesIndex=choicesIndex.filter(item => item !== parseInt(squareId));
-    // origBoard[squareId] = player;
-	// document.getElementById(squareId).innerText = player;
-    // }
-    // else{
-    player=huPlayer;
+	if(document.getElementById("twoPlayers").checked){
+        if(player===player1){
+            player=player2;
+        }else{
+            player=player1;
+        }
+    }else{
+        player=player1;
+    }
+    playSound(player)
     choicesIndex=choicesIndex.filter(item => item !== parseInt(squareId));
     origBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
     document.getElementById(squareId).removeEventListener("click",turnClick, false);
     checkBoard();
-    if(!gameOver){
+    if(!gameOver && document.getElementById("onePlayer").checked){
     computersTurn();
     }
 
@@ -69,9 +85,9 @@ function turn(squareId) {
 
 function computersTurn(){
     if (choicesIndex.length>0){
-    const choice = choicesIndex[Math.floor(Math.random() * choicesIndex.length)]; 
-    origBoard[choice] = aiPlayer;
-    document.getElementById(choice).innerText = aiPlayer;
+    const choice = minimax(origBoard,player2).index;
+    origBoard[choice] = player2;
+    document.getElementById(choice).innerText = player2;
     choicesIndex=choicesIndex.filter(item => item !== choice);
     document.getElementById(choice).removeEventListener("click",turnClick, false);
     }
@@ -91,22 +107,111 @@ function checkBoard(){
         }
         const allEqual = arr => arr.every( v => v === arr[0] )
         if (allEqual(presentCombo)) {
-            comboToCheck.forEach(function(index) {
-                document.getElementById(index).classList.add("won")
-            })
-            gameOver=true;
-            // console.log(presentCombo[0]+" Wins")
-            choicesIndex.forEach(function(index) {
-                document.getElementById(index).removeEventListener("click",turnClick, false);
-            })
-            document.getElementById("endPage").style.display = "flex";
-            document.getElementById("winMessage").innerHTML=presentCombo[0]+ " Wins"
-            break;
+          comboToCheck.forEach(function(index) {
+          document.getElementById(index).classList.add("won")
+        })
+        gameOver=true;
+        choicesIndex.forEach(function(index) {
+          document.getElementById(index).removeEventListener("click",turnClick, false);
+        })
+        document.getElementById("endPage").style.display = "flex";
+        if (presentCombo[0]!==player1){
+          playSound("lost");
+  
+          if(document.getElementById("twoPlayers").checked){
+            document.getElementById("winMessage").innerHTML="Player 2 wins!"
+          }
+          else{
+          document.getElementById("winMessage").innerHTML="Computer wins!"
+          }
+        }
+        else{
+          document.getElementById("winMessage").innerHTML="You win!"
+        }
+        
+        break;
         }
         if (choicesIndex.length===0 && !gameOver){
             gameOver=true;
             document.getElementById("endPage").style.display = "flex";
-            document.getElementById("winMessage").innerHTML="DRAW"
+            document.getElementById("winMessage").innerHTML="DRAW!"
         }
     }
+}
+
+
+function emptyIndexies(board){
+  return  board.filter(s => s != "O" && s != "X");
+}
+
+function winning(board, player){
+  for (var i=0;i<8;i++){
+    let comboToCheck=winCombos[i];
+    let presentCombo=[];
+
+    for (var j=0;j<3;j++){
+       presentCombo.push(board[comboToCheck[j]]);
+ 
+    }
+    const allEqual = arr => arr.every( v => v === arr[0] )
+    if (allEqual(presentCombo)) {
+    if(presentCombo[0]===player){
+      return true;
+    }else {
+      return false;
+    }
+    }
+ 
+}
+}
+
+function minimax(currentBoard, player){
+  var availSpots = emptyIndexies(currentBoard);
+  if (winning(currentBoard, player1)){
+    return {score:-10};
+
+ }
+ else if (winning(currentBoard, player2)){
+   return {score:10};
+ }
+ else if (availSpots.length === 0){
+   return {score:0};
+ }
+ var moves = [];
+
+  for (var i = 0; i < availSpots.length; i++){
+    var move = {};
+  	move.index = currentBoard[availSpots[i]];
+    currentBoard[availSpots[i]] = player;
+    if (player == player2){
+      move.score = minimax(currentBoard, player1).score;
+    }
+    else{
+      move.score = minimax(currentBoard, player2).score;
+    }
+
+    currentBoard[availSpots[i]] = move.index;
+    moves.push(move);
+  }
+  var bestMove;
+  if(player === player2){
+    var bestScore = -Infinity;
+    for(var i = 0; i < moves.length; i++){
+      if(moves[i].score > bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }else{
+
+    var bestScore = Infinity;
+    for(var i = 0; i < moves.length; i++){
+      if(moves[i].score < bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
 }
